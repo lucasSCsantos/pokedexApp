b507adb260169b2df2502e79bc33fc621e3fe760
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-02 16:43:20 -0300

    :sparkles: Create textfield
    create textfield component

---
+++ src/components/textField/index.tsx
@@ -0,0 +1,13 @@
+import { TextInput } from 'react-native';
+import { Container } from './styles';
+
+
+function TextField() {
+  return (
+    <Container>
+      <TextInput>textField</TextInput>
+    </Container>
+  );
+};
+
+export default TextField;
