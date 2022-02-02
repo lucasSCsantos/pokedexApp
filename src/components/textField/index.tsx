b19f874096178ce9f23b5a02cda13fd3150d1b91
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-02 17:20:55 -0300

    :sparkles: Introduce props on text field
    introduce props on text field to make it generic

--- src/components/textField/index.tsx
+++ src/components/textField/index.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { Container, Input } from './styles';
 import Icon from '@expo/vector-icons/FontAwesome5';
 import theme from '../../styles/theme';
 
+interface TextFieldProps {
+  onChangeText: Dispatch<SetStateAction<string>>;
+  value: string;
+}
 
-function TextField() {
+function TextField({ onChangeText, value }: TextFieldProps) {
   const { defaultInput, pressedInput } = theme.color.background
-  const [text, setText] = useState("");
   const [background, setBackground] = useState(defaultInput);
 
   const onFocus = () => {
@@ -21,8 +24,8 @@ function TextField() {
     <Container style={{ backgroundColor: background }}>
       <Icon name="search" size={16} color="#000" />
       <Input 
-        onChangeText={setText}
-        value={text}
+        onChangeText={onChangeText}
+        value={value}
         onFocus={ () => onFocus() }
         onBlur={ () => onBlur() }
         placeholder="What Pokemon are you looking for?"
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,18 @@
 import { Container } from './styles';
 import { Title } from '../../components/typography';
 import TextField from '../../components/textField';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function Home() {
+  const [text, setText] = useState("");
   return (
     <Container>
       <Title color="black">Home</Title>
-      <TextField />
+      <TextField 
+        onChangeText={setText}
+        value={text}
+      />
     </Container>
   );
 };
