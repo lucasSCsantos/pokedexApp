d5cde8bc99eb0c87cd08e05d2a1bf57bd5086a41
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-01 22:32:28 -0300

    :tada: Begin pokedexApp
    Begin project and install some dependencies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{ fontSize: 30 }}>Hello World</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
