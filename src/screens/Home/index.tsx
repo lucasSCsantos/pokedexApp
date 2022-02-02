97e1ba87c35fb76f2110c13e2080b645065a09a5
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-02 17:23:34 -0300

    :coffin: Remove dead components
    remove components that ware made for tests

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,10 @@
 import { Container } from './styles';
 import { Title } from '../../components/typography';
-import TextField from '../../components/textField';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 function Home() {
-  const [text, setText] = useState("");
   return (
     <Container>
       <Title color="black">Home</Title>
-      <TextField 
-        onChangeText={setText}
-        value={text}
-      />
     </Container>
   );
 };
