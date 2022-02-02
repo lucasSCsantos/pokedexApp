bb8133a32ab8900cf42274586e100128675a1a79
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-02 01:01:29 -0300

    :sparkles: Add about and home screen
    add about and home screen on navigation

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,8 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import About from './screens/About';
+
+const Stack = createStackNavigator();
 
 function Navigation() {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
