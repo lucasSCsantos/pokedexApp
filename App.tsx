1bb79a19e707514f2f7549fa67fb0de36864e987
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-01 22:36:37 -0300

    :tada: Begin development
    Begin development branch with useless code

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, Text } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{ fontSize: 30 }}>Hello World</Text>
+      <Text style={{ fontSize: 30 }}>Hello World Dev</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
