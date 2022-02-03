c44ea965928bf51f312a7d784666d9e02e4e9728
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-02 21:23:29 -0300

    :lipstick: Add styles on home
    Add styles on home top area

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,19 @@
-import { Container } from './styles';
-import { ApplicationTitle, Description } from '../../components/typography';
-import TextField from '../../components/textField';
+import { AppTitle, Container, HomeDescription, HomeTextField } from './styles';
 import { useState } from 'react';
 
 function Home() {
   const [text, setText] = useState('');
   const placeholder = "What Pokemon are you looking for?";
-  
+
   return (
     <Container>
-      <ApplicationTitle color="black">
+      <AppTitle color="black">
         Pokédex
-      </ApplicationTitle>
-      <Description color="grey">
+      </AppTitle>
+      <HomeDescription color="grey">
         Search for Pokémon by name or using the National Pokédex number.
-      </Description>
-      <TextField
+      </HomeDescription>
+      <HomeTextField
         onChangeText={setText}
         value={text}
         placeholder={placeholder}
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,22 @@
 import styled from 'styled-components/native';
+import TextField from '../../components/textField';
+import { ApplicationTitle, Description } from '../../components/typography';
 
 export const Container = styled.View`
 	flex: 1;
-	/* align-items: center; */
-	/* justify-content: center; */
 	background-color: ${({ theme }) => theme.color.background.white};
 	padding: 100px 40px 0;
+`;
+
+export const AppTitle = styled(ApplicationTitle)`
+	margin-bottom: 10px;
+`;
+
+export const HomeDescription = styled(Description)`
+	margin-bottom: 25px;
+`;
+
+
+export const HomeTextField = styled(TextField)`
+	margin-bottom: 20px;
 `;
