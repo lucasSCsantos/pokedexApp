abcf67673bb51b367f21a664eda77d1e36776bd7
lucasSCsantos <luc.cristovam10@gmail.com>
2022-02-03 00:56:10 -0300

    :bug: Fix on async logic bug
    Fix on pokemon card to make code await for the req

--- src/components/pokemonsList/PokemonCards.tsx
+++ src/components/pokemonsList/PokemonCards.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import correctNumbers from '../../helpers/correctNumbers';
 import getPokemonDetails from '../../services/getPokemonDetails';
 import Badge from '../badge';
 import { PokemonName, PokemonNumber } from '../typography';
@@ -9,31 +10,43 @@ interface PokemonCardsProps {
 }
 
 function PokemonCards({ name }: PokemonCardsProps) {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState(false);
+  const [id, setId] = useState("");
 
   useEffect(() => {
     const getPokemon = async (name: string) => {
       const details = await getPokemonDetails(name);
       setPokemon(details);
     }
+
     getPokemon(name);
   }, [])
 
+  useEffect(() => {
+    if (pokemon) {
+      setId(correctNumbers(pokemon.id));
+    }
+  }, [pokemon])
+
   return (
-      <CardContainer>
-        <PokemonNumber color="number">#{pokemon.id}</PokemonNumber>
-        <PokemonName color="white">{pokemon.name}</PokemonName>
-        <BadgeContainer>
-          {pokemon.types.map(({ type }) => (
-            <Badge type={type.name} full />
-          ))}
-        </BadgeContainer>
-        <PokemonImage 
-          source={{ 
-            uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id}.png` 
-          }} 
-        />
-      </CardContainer>
+    <CardContainer>
+      { pokemon && (
+        <>
+          <PokemonNumber color="number">#{id}</PokemonNumber>
+          <PokemonName color="white">{pokemon.name}</PokemonName>
+          <BadgeContainer>
+            {pokemon.types.map(({ type }) => (
+              <Badge type={type.name} full />
+            ))}
+          </BadgeContainer>
+          <PokemonImage 
+            source={{ 
+              uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png` 
+            }} 
+          />
+        </>
+      )}
+    </CardContainer>
   );
 }
 
